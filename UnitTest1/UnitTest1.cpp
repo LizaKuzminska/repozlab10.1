3532ab74620bda8d92098ab5cdf72c99e103c923
LizaKuzminska <yelyzavetakuzminska@gmail.com>
2023-12-13 18:24:42 +0200

    dbvdz

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab10.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			using namespace std;
+			string fileName = "test.txt";
+			int result = hasAdjacentPairs(fileName);
+
+			ofstream file(fileName);
+			file << "aabbccdd\n";
+			file.close();
+
+			Assert::AreEqual(result, 1);
+		}
+	};
+}
